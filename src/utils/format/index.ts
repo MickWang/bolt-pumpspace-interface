7d973541e5d8ecc67e235ab58050f5d0d76f79fe
MickWang <1244134672@qq.com>
2024-12-27 15:49:42 +0800

    Commit from Bolt to GitHub

--- src/utils/format/index.ts
+++ src/utils/format/index.ts
@@ -1,5 +1,6 @@
 // Export all format utilities
 export { formatAddress, formatSmartMoneyName } from './address';
 export { formatNumber, formatDecimal } from './number';
-export { formatPrice, formatSmallDecimals } from './price';
+export { formatPrice, formatSmallDecimals, formatCurrency } from './price';
 export { formatTimeAgo } from './time';
+export { formatMC, calculateTokenMC } from './marketCap';
--- src/utils/format/price.ts
+++ src/utils/format/price.ts
@@ -1,3 +1,12 @@
+export function formatCurrency(value: number): string {
+  return new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2
+  }).format(value);
+}
+
 export function formatSmallDecimals(
   input: string | number,
   decimalPlaces: number = 4
